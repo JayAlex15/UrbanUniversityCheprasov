9ec9dead77d5cf5f4f6c363677d0bc5bfe1de8b5
JayAlex15 <a.k.cheprasov@gmail.com>
2024-12-16 10:28:06 +0300

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,12 @@
+def add_everything_up(a, b):
+    try:
+        result = a + b
+    except TypeError:
+        a = str(a)
+        b = str(b)
+        result = a + b
+    return result
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
